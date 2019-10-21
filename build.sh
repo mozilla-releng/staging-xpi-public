bb000717f122dd896b55fa9199be47bd0b2b0f0f
Aki Sasaki <aki@escapewindow.com>
2019-10-21 11:42:23 -0700

    rename built xpi

--- build.sh
+++ build.sh
@@ -9,13 +9,10 @@ BASE_DIR="$(dirname "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)")"
 #DEST="${TMP_DIR}/addon"
 ADDON_VERSION=$(node -p -e "require('./package.json').version");
 ADDON_ID=$(node -p -e "require('./package.json').addon.id")
-XPI_NAME="${ADDON_ID}-${ADDON_VERSION}".xpi
+XPI_NAME=target.xpi
 
 pushd addon > /dev/null
 zip -r  "../${XPI_NAME}" .
 popd > /dev/null
 
-rm -f linked-addon.xpi
-ln -s "${XPI_NAME}" linked-addon.xpi
 echo "SUCCESS: xpi at ${XPI_NAME}"
-echo "SUCCESS: symlinked xpi at linked-addon.xpi"
