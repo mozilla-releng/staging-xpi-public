735cf308e4aedd401a98b2bae1e4c8d8987a3e16
Aki Sasaki <aki@escapewindow.com>
2019-10-21 17:49:26 -0700

    latest

--- taskcluster/xpi_taskgraph/test.py
+++ taskcluster/xpi_taskgraph/test.py
@@ -12,7 +12,7 @@ import json
 import os
 
 from taskgraph.transforms.base import TransformSequence
-from xpi_taskgraph.xpi_manifest import get_manifest
+from xpi_taskgraph.utils import get_manifest
 
 
 transforms = TransformSequence()
@@ -26,47 +26,28 @@ def test_tasks_from_manifest(config, tasks):
         task["attributes"] = dep.attributes.copy()
         task["dependencies"] = {"build": dep.label}
         xpi_name = dep.task["extra"]["xpi-name"]
-        if xpi_name:
-            xpi_revision = config.params.get('xpi_revision')
         task.setdefault("extra", {})["xpi-name"] = xpi_name
-        for xpi_config in manifest.get("xpis", []):
-            if not xpi_config.get("active"):
-                continue
+        for xpi_config in manifest:
             if xpi_config["name"] == xpi_name:
                 break
         else:
             raise Exception("Can't determine the upstream xpi_config for {}!".format(xpi_name))
         env = task.setdefault("worker", {}).setdefault("env", {})
         run = task.setdefault("run", {})
-        checkout = run.setdefault("checkout", {})
-        checkout_config = checkout.setdefault(xpi_config['repo-prefix'], {})
-        env['REPO_PREFIX'] = xpi_config['repo-prefix']
-        checkout_config['path'] = '/builds/worker/checkouts/src'
         if 'directory' in xpi_config:
             run['cwd'] = '{checkout}/%s' % xpi_config['directory']
-        if xpi_revision:
-            checkout_config['head_rev'] = xpi_revision
+        else:
+            run['cwd'] = '{checkout}'
         task["label"] = "test-{}".format(xpi_name)
         task["treeherder"]["symbol"] = "T({})".format(
             xpi_config.get("treeherder-symbol", xpi_config["name"])
         )
-        if xpi_config.get("private-repo"):
+        if xpi_config.get("github_clone_secret"):
             checkout_config['ssh_secret_name'] = config.graph_config["github_clone_secret"]
             artifact_prefix = "xpi/build"
             task["worker"]["taskcluster-proxy"] = True
         else:
             artifact_prefix = "public/build"
         env["ARTIFACT_PREFIX"] = artifact_prefix
-
-        paths = []
-        for artifact in xpi_config["artifacts"]:
-            artifact_name = "{}/{}".format(
-                artifact_prefix, os.path.basename(artifact)
-            )
-            paths.append(artifact_name)
-        upstreamArtifacts = [
-            {"taskId": "<build>", "paths": paths},
-        ]
-        env["XPI_UPSTREAM_URLS"] = json.dumps(upstreamArtifacts)
 
         yield task
